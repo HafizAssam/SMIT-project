541ff5d4eb39db7e10e5412d19788a6deaa07fb1
HafizAssam <assamchohan@gmail.com>
2023-07-21 21:12:59 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,57 @@
+import 'dart:io';
+
+void main() {
+  print("Welcome to SMIT Restaurant");
+  print('*    ============    *');
+  print("");
+  print('press 1 for menu');
+  print('press 2 for feedback');
+  print('press 3 for exit');
+  var userinput = stdin.readLineSync();
+  if (userinput == "1") {
+    menu();
+  } else if (userinput == "2") {
+    feedback();
+  } else {}
+}
+
+List enterfeedback = [];
+menu() {
+  print("SMIT Restaurant Menu");
+  print('');
+  print("press 1 for Fast Food menu");
+  print("press 2 for BBQ menu");
+  print("press 3 for Rolls menu");
+  print("press 4 for back menu");
+  var menuinput = stdin.readLineSync();
+  if (menuinput == "1") {
+    FastFood();
+  } else if (menuinput == "2") {
+    BBQ();
+  } else if (menuinput == "3") {
+    Rolls();
+  } else {
+    main();
+  }
+}
+
+FastFood() {
+  print("Fast Food Menu");
+}
+
+BBQ() {
+  print("BBQ Menue");
+}
+
+Rolls() {
+  print("Rools Menue");
+}
+
+feedback() {
+  print("please enter your feedback");
+  var feedBack = stdin.readLineSync();
+  enterfeedback.add(feedBack);
+  print('$feedBack');
+}
+
+exit() {}
